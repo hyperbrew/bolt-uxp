ce1e9235e9f69381c50bf671a5ba782306177984
Justin Taylor <justin@justintaylor.tv>
2023-10-23 12:55:49 -0700

    plugin abstract

---
+++ vite-uxp-plugin/index.ts
@@ -0,0 +1,59 @@
+import type { OutputChunk } from "rollup";
+import type { Plugin } from "vite";
+import os from "os";
+import { execSync } from "child_process";
+
+export const polyfills = `(function(){var t;null==window.MutationObserver&&(t=function(){function t(t){this.callBack=t}return t.prototype.observe=function(t,n){var e;return this.element=t,this.interval=setInterval((e=this,function(){var t;if((t=e.element.innerHTML)!==e.oldHtml)return e.oldHtml=t,e.callBack.apply(null)}),200)},t.prototype.disconnect=function(){return window.clearInterval(this.interval)},t}(),window.MutationObserver=t)}).call(this);`;
+
+export const uxp = (): Plugin => {
+  return {
+    name: "vite-uxp-plugin",
+    generateBundle(output, bundle) {
+      Object.keys(bundle)
+        .filter((file) => file.indexOf(".js") > 0)
+        .map((file) => {
+          const current = bundle[file] as OutputChunk;
+          current.code = polyfills + "\n" + current.code;
+        });
+    },
+    transformIndexHtml(html) {
+      return html.replace('<script type="module" crossorigin', "<script");
+    },
+  };
+};
+
+const upiaMac =
+  "/Library/Application Support/Adobe/Adobe Desktop Common/RemoteComponents/UPI/UnifiedPluginInstallerAgent/UnifiedPluginInstallerAgent.app/Contents/MacOS/UnifiedPluginInstallerAgent";
+const upiaWin =
+  "C:/Program Files/Common Files/Adobe/Adobe Desktop Common/RemoteComponents/UPI/UnifiedPluginInstallerAgent/UnifiedPluginInstallerAgent.exe";
+
+const upiaPath = os.platform() === "darwin" ? upiaMac : upiaWin;
+
+export const runAction = (opts: object, action: string) => {
+  const outPath = process.cwd() + "/dist";
+  console.log("outPath", outPath);
+
+  if (action === "install") {
+    //* Install a CCX Plugin Package
+    console.log("install");
+    const res = execSync(`"${upiaPath}" /install "${outPath}"`, {
+      encoding: "utf-8",
+    });
+    console.log("res", res);
+    // makeSymlink(symlinkSrc, symlinkDst);
+  } else if (action === "uninstall") {
+    //* Uninstall a CCX Plugin Package
+    // removeSymlink(symlinkSrc, symlinkDst);
+    const res = execSync(`"${upiaPath}" /remove "${outPath}"`, {
+      encoding: "utf-8",
+    });
+    console.log("res", res);
+  } else if (action === "startdebug") {
+    //* Start Debugging a development Plugin Package
+  } else if (action === "stopdebug") {
+    //* Stop Debugging a development Plugin Package
+  } else {
+    console.warn(`Unknown Action: ${action}`);
+  }
+  //   resetLog();
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import banner from "vite-plugin-banner";
-import type { OutputChunk } from "rollup";
-import type { Plugin } from "vite";
+import { runAction, uxp } from "./vite-uxp-plugin/index";
 
-// import { mutationObserverPolyfill } from "./src/polyfill";
-export const polyfills = `(function(){var t;null==window.MutationObserver&&(t=function(){function t(t){this.callBack=t}return t.prototype.observe=function(t,n){var e;return this.element=t,this.interval=setInterval((e=this,function(){var t;if((t=e.element.innerHTML)!==e.oldHtml)return e.oldHtml=t,e.callBack.apply(null)}),200)},t.prototype.disconnect=function(){return window.clearInterval(this.interval)},t}(),window.MutationObserver=t)}).call(this);`;
+const action = process.env.ACTION;
 
-function viteUXPPlugin(): Plugin {
-  return {
-    name: "vite-uxp-plugin",
-    generateBundle(output, bundle) {
-      Object.keys(bundle)
-        .filter((file) => file.indexOf(".js") > 0)
-        .map((file) => {
-          const current = bundle[file] as OutputChunk;
-          current.code = polyfills + "\n" + current.code;
-        });
-    },
-    transformIndexHtml(html) {
-      return html.replace('<script type="module" crossorigin', "<script");
-    },
-  };
+if (action) {
+  runAction({}, action);
+  process.exit();
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteUXPPlugin(), svelte()],
+  plugins: [uxp(), svelte()],
 });
