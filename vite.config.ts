94912e712d56cfdfe598e63b13d4c91a20877110
Justin Taylor <justin@justintaylor.tv>
2023-10-23 22:16:18 -0700

    consolidated svelte configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 // @ts-ignore
 import { runAction, uxp } from "./vite-uxp-plugin/index";
+import { sveltePreprocess } from "svelte-preprocess/dist/autoProcess";
 
 const action = process.env.ACTION;
 
@@ -12,7 +13,12 @@ if (action) {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uxp(), svelte()],
+  plugins: [
+    uxp(),
+    svelte({
+      preprocess: sveltePreprocess({ typescript: true }),
+    }),
+  ],
   // externals
   build: {
     rollupOptions: {
