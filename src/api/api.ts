d9497ca0d53ba15e29dce4d75e9abc0602411de9
Justin Taylor <justin@justintaylor.tv>
2023-12-18 11:38:39 -0800

    account for beta in appname

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,9 @@ import * as indesgin from "./indesign"; // BOLT-UXP_INDESIGN_ONLY
 import * as premierepro from "./premierepro"; // BOLT-UXP_PREMIEREPRO_ONLY
 import * as illustrator from "./illustrator"; // BOLT-UXP_ILLUSTRATOR_ONLY
 
-const hostName = require("uxp").host.name.toLowerCase() as string;
+const hostName = require("uxp")
+  .host.name.toLowerCase()
+  .replace(/\s/g, "") as string;
 
 // prettier-ignore
 let host = {} as 
@@ -12,9 +14,9 @@ let host = {} as
   & typeof premierepro // BOLT-UXP_PREMIEREPRO_ONLY
   & typeof illustrator; // BOLT-UXP_ILLUSTRATOR_ONLY
 
-if (hostName === "photoshop") host = photoshop; // BOLT-UXP_PHOTOSHOP_ONLY
-if (hostName === "indesign") host = indesgin; // BOLT-UXP_INDESIGN_ONLY
-if (hostName === "premierepro") host = premierepro; // BOLT-UXP_PREMIEREPRO_ONLY
-if (hostName === "illustrator") host = illustrator; // BOLT-UXP_ILLUSTRATOR_ONLY
+if (hostName.startsWith("photoshop")) host = photoshop; // BOLT-UXP_PHOTOSHOP_ONLY
+if (hostName.startsWith("indesign")) host = indesgin; // BOLT-UXP_INDESIGN_ONLY
+if (hostName.startsWith("premierepro")) host = premierepro; // BOLT-UXP_PREMIEREPRO_ONLY
+if (hostName.startsWith("illustrator")) host = illustrator; // BOLT-UXP_ILLUSTRATOR_ONLY
 
 export const api = host;
