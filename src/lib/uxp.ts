75a926c76b6fb1c1bf2e8ffa743ef231a82a7a0d
Justin Taylor <justin@justintaylor.tv>
2024-02-21 12:15:20 -0800

    0.1.12 fix openUXPPanel()

--- src/lib/uxp.ts
+++ src/lib/uxp.ts
@@ -1,7 +1,11 @@
-import { uxp } from "../globals";
-
 export const openUXPPanel = async (id: string) => {
-  const plugin = Array.from(uxp.pluginManager.plugins).pop();
-  //@ts-ignore
+  const uxp = require("uxp") as typeof import("uxp");
+  const plugins = Array.from(uxp.pluginManager.plugins);
+
+  const plugin = plugins.find(
+    (plugin) => plugin.id === uxp.entrypoints._pluginInfo.id
+  );
+  console.log("plugin", plugin, "opening panel: ", id);
   if (plugin) await plugin.showPanel(id);
+  else console.error("No plugin found");
 };
