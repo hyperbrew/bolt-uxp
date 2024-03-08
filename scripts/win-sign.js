364fc72b3c4d3dc3cd03dc2a38f36c34fd8621c8
Justin Taylor <justin@justintaylor.tv>
2024-03-07 16:42:05 -0800

    windows signing added

---
+++ scripts/win-sign.js
@@ -0,0 +1,48 @@
+import { execSync } from "child_process";
+import dotenv from "dotenv";
+dotenv.config();
+
+const {
+  AZURE_CERT_NAME,
+  AZURE_URL,
+  AZURE_CLIENT_ID,
+  AZURE_TENANT_ID,
+  AZURE_SECRET_VALUE,
+} = process.env;
+
+if (
+  !AZURE_CERT_NAME ||
+  !AZURE_URL ||
+  !AZURE_CLIENT_ID ||
+  !AZURE_TENANT_ID ||
+  !AZURE_SECRET_VALUE
+) {
+  console.log({
+    AZURE_CERT_NAME,
+    AZURE_URL,
+    AZURE_CLIENT_ID,
+    AZURE_TENANT_ID,
+    AZURE_SECRET_VALUE,
+  });
+  console.error("Please provide all required environment variables");
+  process.exit(1);
+}
+
+const sign = (binPath) => {
+  console.log(`Signing ${binPath}`);
+  const res = execSync(
+    `azuresigntool sign -kvu "${AZURE_URL}" -kvi "${AZURE_CLIENT_ID}" -kvt "${AZURE_TENANT_ID}"  -kvs "${AZURE_SECRET_VALUE}" -kvc "${AZURE_CERT_NAME}" -tr http://timestamp.digicert.com -v "${binPath}"`,
+    {
+      encoding: "utf-8",
+    }
+  );
+  console.log("Win Sign result:");
+  console.log(res);
+  return;
+};
+
+const main = async () => {
+  await sign("./public-hybrid/win/x64/bolt-uxp-hybrid.uxpaddon");
+  await sign("./public-hybrid/win/arm64/bolt-uxp-hybrid.uxpaddon");
+};
+main();
