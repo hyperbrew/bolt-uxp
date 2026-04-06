6bf51b42e476e6f074deab65ed9157c0b27c290f
Kacper Majorkowski <kmkacpermajor@gmail.com>
2026-04-06 22:10:54 +0200

    init (#49)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import * as photoshop from "./photoshop"; // BOLT_PHXS_ONLY
-import * as indesgin from "./indesign"; // BOLT_IDSN_ONLY
+import * as indesign from "./indesign"; // BOLT_IDSN_ONLY
 import * as premierepro from "./premierepro"; // BOLT_PPRO_ONLY
 import * as illustrator from "./illustrator"; // BOLT_ILST_ONLY
 import { uxp } from "../globals";
@@ -12,14 +12,14 @@ const hostName =
 let host = {} as 
   & typeof uxpLib // BOLT_PHXS_ONLY
   & typeof photoshop // BOLT_PHXS_ONLY
-  & typeof indesgin // BOLT_IDSN_ONLY
+  & typeof indesign // BOLT_IDSN_ONLY
   & typeof premierepro // BOLT_PPRO_ONLY
   & typeof illustrator; // BOLT_ILST_ONLY
 
 export type API = typeof host & typeof uxpLib;
 
 if (hostName.startsWith("photoshop")) host = photoshop; // BOLT_PHXS_ONLY
-if (hostName.startsWith("indesign")) host = indesgin; // BOLT_IDSN_ONLY
+if (hostName.startsWith("indesign")) host = indesign; // BOLT_IDSN_ONLY
 if (hostName.startsWith("premierepro")) host = premierepro; // BOLT_PPRO_ONLY
 if (hostName.startsWith("illustrator")) host = illustrator; // BOLT_ILST_ONLY
 
